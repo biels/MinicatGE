3606e57e57d7263bb24893a073d3e51c76a09bf3
Biel Simon Rojas <bielsimon2@gmail.com>
2017-01-31 07:09:23 +0100

    Imported Spigot and finished base pluigin

---
+++ src/main/java/com/biel/minicat/ge/Com.java
@@ -0,0 +1,7 @@
+package com.biel.minicat.ge;
+
+/**
+ * Created by Biel on 31/1/2017.
+ */
+public class Com {
+}
--- src/main/java/com/biel/minicat/ge/MinicatGEPlugin.java
+++ src/main/java/com/biel/minicat/ge/MinicatGEPlugin.java
@@ -1,7 +1,14 @@
 package com.biel.minicat.ge;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by Biel on 31/1/2017.
  */
-public class MinicatGEPlugin {
+public class MinicatGEPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
 }
